d0107206675f127c5bc93cf6dd2d9134dafad246
Pratik1803 <pratikvaidya1803@gmail.com>
2022-04-25 20:08:50 +0530

    added cors for .get/kanjis

--- routes/kanji.js
+++ routes/kanji.js
@@ -1,23 +1,24 @@
 const express = require("express");
-const router = new express.Router;
-const Kanjis = require('../models/Kanji');
+const router = new express.Router();
+const cors = require("cors");
+const Kanjis = require("../models/Kanji");
 
-router.post("/add_kanji", async (req,res)=>{
-    try {
-        const result = await Kanjis(req.body).save();
-        res.status(201).send(result);
-    } catch (error) {
-        console.log(error);
-    }
+router.post("/add_kanji", async (req, res) => {
+	try {
+		const result = await Kanjis(req.body).save();
+		res.status(201).send(result);
+	} catch (error) {
+		console.log(error);
+	}
 });
 
-router.get("/kanjis", async (req,res)=>{
-    try {
-        const result = await Kanjis.find();
-        res.status(200).send(result);
-    } catch (error) {
-        console.log(error);
-    }
-})
+router.get("/kanjis", cors({ origin: true }), async (req, res) => {
+	try {
+		const result = await Kanjis.find();
+		res.status(200).send(result);
+	} catch (error) {
+		console.log(error);
+	}
+});
 
 module.exports = router;
