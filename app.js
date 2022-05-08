6418d3c045542b965e4e1208852f7d0fb4eec535
Pratik1803 <pratikvaidya1803@gmail.com>
2022-05-08 15:51:46 +0530

    tackled cors

--- app.js
+++ app.js
@@ -10,35 +10,19 @@ const addKanjis = require("./routes/kanji");
 const user = require("./routes/user");
 const jwt = require("jsonwebtoken");
 
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(addKanjis);
 app.use(user);
 
-// const createToken = async ()=>{
-// 	try {
-// 		const token = await jwt.sign({_id:"12345678910"}, "mynameispratikvaidyamernstackdeveloper")
-// 		console.log(token);
-// 		const ver = await jwt.verify(token, "mynameispratikvaidyamernstackdeveloper");
-// 		console.log(ver);
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
-
-// createToken();
-
-// if (process.env.NODE_ENV == "production") {
-// 	app.use(express.static("client/build"));
-// 	const path = require("path");
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// 	});
-// }
-
-app.get("/", (req,res)=>{
-	res.json({status:200, message:"Hello from backend!"});
-})
+if (process.env.NODE_ENV == "production") {
+	app.use(express.static("client/build"));
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}...`);
